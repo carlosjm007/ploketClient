7919d89796bfcfa32bdb004270d9ca64f78d6916
carlosj.manosalvar@gmail.com <renault4>
2018-05-15 12:17:25 -0500

    Se configura para andrioid

--- Herramientas/Configuracion.cs
+++ Herramientas/Configuracion.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Configuracion {
 	public bool ps4=false;
 	public bool xbox_one=false;
-	public bool pc=true;
+	public bool pc=false;
 	public bool web=false;
-	public bool android=false;
+	public bool android=true;
 	public bool ios=false;
 }
